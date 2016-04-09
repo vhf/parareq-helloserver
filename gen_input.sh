712c494f9b3cce7dbdb1f9bc7675e47eb49d1cc3
Victor Felder <victorfelder@gmail.com>
2016-04-09 15:49:14 +0200

    Generate precise number of inputs

--- gen_input.sh
+++ gen_input.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
-echo "generating input for $1 servers"
+rm -f tmp_input
+n=$1
+total=50000
+echo "generating $total inputs for $1 servers"
 start=1337
-end=$(($1 + 1337))
+end=$((n + 1337 - 1))
+ports="$(seq $start 1 $end)"
 
-for i in `seq 100000`; do
-  for port in `seq $start 1 $end`; do
-    echo "xbhttp://localhost:$port" >> tmp_input;
+expected=$((total/n))
+got=$((expected * n))
+diff=$((total - got))
+new_end=$((diff + 1337 - 1))
+
+for i in `seq $expected`; do
+  for port in $ports; do
+    echo "x|http://localhost:$port" >> tmp_input;
   done
 done
+if [[ $diff -gt $zero ]]; then
+  extra_ports=$(seq $start 1 $new_end)
+  for port in $extra_ports; do
+    echo "x|http://localhost:$port" >> tmp_input;
+  done
+fi
 
+tr '|' '\t' < tmp_input > input
 rm tmp_input
-tr 'b' '\t' < tmp_input > input
